0ad74f9636ef40cae104201b9d6232d9b4f5aeb6
schirmerp <schirmer.pc@gmail.com>
2022-04-08 00:53:11 -0400

    big changes

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 const container = document.querySelector(".container")
-//const element = document.querySelector('#back')
 const  main = document.createElement("div")
-//import { back } from './bkspc.js'
-
-container.appendChild(main)
-main.style.backgroundColor = 'lightGrey'
-main.style.height = '700px'
-main.style.width = '600px'
-main.className = 'main'
 
 const sentences = ["We are smart!", "We are Learning!", "We are smarter!"]
-
 
 fetch("./class.json")
 .then(response => {
@@ -18,29 +9,120 @@ fetch("./class.json")
        console.log(data)
        for (i in data){
            const card = document.createElement('div')
-           card.innerHTML = `${data[i].Name}` + '<br>' + `${data[i].Role}` + '<br>'
-           card.style.border = 'solid black 5px'
-           card.style.padding = '3px'
-           main.appendChild(card)
-           card.style.margin = '3px'
-        }
+           card.classList = 'card'
+           container.appendChild(card)
+
+           const profileCard = document.createElement('div')
+           profileCard.classList = "profile-card"
+           card.appendChild(profileCard)
+  
+           const cardHeader = document.createElement('div')
+           cardHeader.classList="card-header"
+           profileCard.appendChild(cardHeader)
+  
+           const picDiv = document.createElement('div')
+           picDiv.classList="picDiv"
+           cardHeader.appendChild(picDiv)
+  
+           const picImg = document.createElement('img')
+           picImg.classList = "pic"
+           picImg.src = `${data[i].pic}`
+           picImg.alt = "pic"
+           cardHeader.appendChild(picImg)
+  
+           const nameDiv = document.createElement('div')
+           nameDiv.classList="name"
+           nameDiv.textContent = `${data[i].Name}`
+           profileCard.appendChild(nameDiv)
+  
+           const descDiv = document.createElement('div')
+           descDiv.classList="desc"
+           descDiv.textContent = `${data[i].Role}`
+           profileCard.appendChild(descDiv)
+  
+           const smDiv = document.createElement('div')
+           smDiv.classList = "sm"
+           profileCard.appendChild(smDiv)
+  
+           const linkFace = document.createElement('a')
+           linkFace.href = `${data[i].link.facebook}`
+           linkFace.classList="fab fa-facebook-f"
+           smDiv.appendChild(linkFace)
+  
+           const linkTwitter = document.createElement('a')
+           linkTwitter.href = `${data[i].link.twitter}`
+           linkTwitter.classList="fab fa-twitter"
+           smDiv.appendChild(linkTwitter)
+  
+           const linkGithub = document.createElement('a')
+           linkGithub.href = `${data[i].link.github}`
+           linkGithub.classList="fab fa-github"
+           smDiv.appendChild(linkGithub)
+  
+           const linkYoutube = document.createElement('a')
+           linkYoutube.href = `${data[i].link.youtube}`
+           linkYoutube.classList="fab fa-linkedin"
+           smDiv.appendChild(linkYoutube)
+ 
+         }
    });
 })
-//back(sentences, 'back')
+const run = (() =>{
+   const testString = ["Brilliant People!", "Learning more everday!", "the Future!", "PerScholas!"]
 
-//document.write( ' <div class=\"profile-card\">\n' );
-//document.write( '    <div class=\"card-header\">\n' );
-//document.write( '      <div class=\"pic\">\n' );
-//document.write( `        <img src=\"${data.link.pic}\" alt=\"\">\n` );
-//document.write( '      </div>\n' );
-//document.write( `      <div class=\"name\">${data.Name}</div>\n` );
-//document.write( `      <div class=\"desc\">${data.Role}</div>\n` );
-//document.write( '      <div class=\"sm\">\n' );
-//document.write( `        <a href=\"${data.link.facebook}\" class=\"fab fa-facebook-f\"></a>\n` );
-//document.write( `        <a href=\"${data.link.twitter}\" class=\"fab fa-twitter\"></a>\n` );
-//document.write( `        <a href=\"${data.link.github}\" class=\"fab fa-github\"></a>\n` );
-//document.write( `        <a href=\"${data.link.youtube}\" class=\"fab fa-youtube\"></a>\n` );
-//document.write( '      </div>\n' );
-//document.write( '      <a href=\"#\" class=\"contact-btn\">Contact Me</a>\n' );
-//document.write( '    </div>\n' );
-//document.write( '  </div>' );
+   const testElement = 'back'
+   
+   function back(strings, element){
+   const textDisplay = document.getElementById(element)
+   const phrases = strings
+   
+   let i = 0
+   let j = 0
+   let currentPhrase = []
+   let isDeleting = false
+   let isEnd = false
+   
+   function backspace() {
+       isEnd = false
+       textDisplay.innerHTML = currentPhrase.join("")
+       
+       if (i < phrases.length) {
+       
+           
+           if (!isDeleting && j <= phrases[i].length){
+               currentPhrase.push(phrases[i][j])
+               j++
+               textDisplay.innerHTML = currentPhrase.join("")
+           }
+   
+           if(isDeleting && j <= phrases[i].length){
+               currentPhrase.pop(phrases[i][j])
+                   j--
+                   textDisplay.innerHTML = currentPhrase.join("")
+           }
+   
+           if (j == phrases[i].length){
+               isEnd = true
+               isDeleting = true
+           }
+           
+           if(isDeleting && j === 0) {
+               currentPhrase = []
+               isDeleting = false
+               i++
+               if(i=== phrases.length){
+                   i= 0
+               }
+           }
+           
+       }   
+       const spedUp = Math.random() * (80 - 50) + 50
+       const normalSpeed = Math.random() * (300 - 200) + 200
+       const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+       setTimeout(backspace, time)
+   }
+   
+   backspace()
+   }
+   back(testString, testElement)
+   })()
